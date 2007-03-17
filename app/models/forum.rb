b35cf6010ae578114f6df60a76fb6b1a33438b02
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-03-17 21:26:44 +0000

    * Rolled back forum changes
    git-svn-id: svn://donmai.us/danbooru/trunk@177 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/forum.rb
+++
@@ -1,2 +0,0 @@
-class Forum < ActiveRecord::Base
-end
--- db/migrate/005_add_created_at_field_to_artists.rb
+++
@@ -1,9 +0,0 @@
-class AddCreatedAtFieldToArtists < ActiveRecord::Migration
-  def self.up
-    execute("ALTER TABLE artists ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT now()")
-  end
-
-  def self.down
-    execute("ALTER TABLE artists DROP COLUMN created_at")
-  end
-end
