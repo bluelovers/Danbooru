adbfa4bd3df929fde4db5d697db738d024804ada
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-03-23 18:53:49 +0000

    * Updated views for commentcontroller changes
    git-svn-id: svn://donmai.us/danbooru/trunk@205 9af9e794-c952-11db-92b5-8e906a41d509

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -2,7 +2,7 @@ module CommentHelper
 	def print_comment(c)
 		txt = h(c.body)
 		txt.gsub!(/&gt;&gt;\d+/) do |match|
-			content_tag("div", link_to("&gt;&gt;#{match[8..-1]}", {:controller => "comment", :action => "view", :id => match[8..-1]}, :onclick => "this.parentNode.innerHTML = $('cbody" + match[8..-1] + "').innerHTML; return false"), :class => "comment-quote")
+			content_tag("div", link_to("&gt;&gt;#{match[8..-1]}", {:controller => "comment", :action => "show", :id => match[8..-1]}, :onclick => "this.parentNode.innerHTML = $('cbody" + match[8..-1] + "').innerHTML; return false"), :class => "comment-quote")
 		end
 		txt.gsub!("\n", "<br/>")
 		txt.gsub!(/&lt;(?:s|spoilers|spoiler)&gt;(.+?)&lt;\/(?:s|spoilers|spoiler)&gt;/, '<a href="#" class="spoilers">\1</a>')
