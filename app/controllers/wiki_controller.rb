f11e68f07696847f57fd09085191088de553fbdf
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-03-23 22:16:35 +0000

    * Wiki fixes
    git-svn-id: svn://donmai.us/danbooru/trunk@210 9af9e794-c952-11db-92b5-8e906a41d509

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -114,10 +114,10 @@ class WikiController < ApplicationController
 	end
 
 	def edit
-		@page = WikiPage.find_page(params[:title], params[:version]) || WikiPage.new(:title => params[:title])
+		@wiki_page = WikiPage.find_page(params[:title], params[:version]) || WikiPage.new(:title => params[:title])
 
-		if @page.new_record? && (Tag.find_by_name(@page.title).tag_type == Tag::TYPE_ARTIST rescue false)
-			@page.body =<<-EOL
+		if @wiki_page.new_record? && (Tag.find_by_name(@wiki_page.title).tag_type == Tag::TYPE_ARTIST rescue false)
+			@wiki_page.body =<<-EOL
 Artist.
 
 Japanese name: 
@@ -127,7 +127,7 @@ h4. See also
 * "Home page":
 			EOL
 		end
-		set_title @page.pretty_title + " (Editing)"
+		set_title @wiki_page.pretty_title + " (Editing)"
 	end
 
 	def revert
