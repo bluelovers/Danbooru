a4b989acd492f966224ceda491d59fdcb176232d
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-05-04 22:41:20 +0000

    * Removed some unnecessary indexes
    git-svn-id: svn://donmai.us/danbooru/trunk@347 9af9e794-c952-11db-92b5-8e906a41d509

---
+++ db/migrate/024_drop_extra_indexes_on_artists.rb
@@ -0,0 +1,13 @@
+class DropExtraIndexesOnArtists < ActiveRecord::Migration
+	def self.up
+		execute "DROP INDEX idx_artists__image_url"
+		execute "DROP INDEX idx_artists__site_url"
+		execute "DROP INDEX idx_favorites__post_user"
+	end
+
+	def self.down
+		execute "CREATE INDEX idx_artists__image_url ON artists (url_b)"
+		execute "CREATE INDEX idx_artists__site_url ON artist (url_a)"
+		execute "CREATE INDEX idx_favorites__post_user ON favorites (post_id, user_id)"
+	end
+end
