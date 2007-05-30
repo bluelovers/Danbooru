118711c8774b4750b10b2ccf4ed5013c3553bde5
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-05-30 03:25:29 +0000

    * Fixed a migration
    git-svn-id: svn://donmai.us/danbooru/trunk@370 9af9e794-c952-11db-92b5-8e906a41d509

--- db/migrate/013_drop_is_ambiguous_field_from_tags.rb
+++ db/migrate/013_drop_is_ambiguous_field_from_tags.rb
@@ -1,9 +1,7 @@
 class DropIsAmbiguousFieldFromTags < ActiveRecord::Migration
 	def self.up
-		execute "ALTER TABLE tags DROP COLUMN is_ambiguous"
 	end
 
 	def self.down
-		execute "ALTER TABLE tags ADD COLUMN is_ambiguous BOOLEAN NOT NULL DEFAULT FALSE"
 	end
 end
