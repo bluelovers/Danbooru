ef5badc4ff33df01e0f91794012f38466b0be541
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-05-30 02:47:58 +0000

    * Fixed migrations
    git-svn-id: svn://donmai.us/danbooru/trunk@366 9af9e794-c952-11db-92b5-8e906a41d509

--- db/migrate/020_change_artists_a.rb
+++ db/migrate/020_change_artists_a.rb
@@ -11,22 +11,6 @@ class ChangeArtistsA < ActiveRecord::Migration
 			execute "ALTER TABLE artists ADD COLUMN url_c TEXT"
 			execute "ALTER TABLE artists ADD COLUMN name TEXT NOT NULL DEFAULT ''"
 			execute "ALTER TABLE artists ALTER COLUMN name DROP DEFAULT"
-
-			artists.each do |artist|
-				reference_name = artist.name
-
-				execute ActiveRecord::Base.sanitize_sql(["INSERT INTO artists (name, url_a, url_b) VALUES (?, ?, ?)", reference_name, artist.site_url, artist.image_url])
-
-				%w(personal_name handle_name circle_name japanese_name).each do |n|
-					name = artist.send(n)
-
-					if ActiveRecord::Base.connection.select_value(ActiveRecord::Base.sanitize_sql(["SELECT 1 FROM artists WHERE name = ?", name])) == nil
-						if !name.blank? && name != reference_name
-							execute ActiveRecord::Base.sanitize_sql(["INSERT INTO artists (name, alias_id) VALUES (?, (SELECT id FROM artists WHERE name = ?))", name, reference_name])
-						end
-					end
-				end
-			end
 		end
 	end
 
