2ef1370aa7a27d7706a7662be6d0fff6161409ff
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-08-17 17:34:10 +0000

    * Added pool links in post/show
    git-svn-id: svn://donmai.us/danbooru/trunk@515 9af9e794-c952-11db-92b5-8e906a41d509

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -27,6 +27,19 @@ module PostHelper
     return html
   end
 
+	def pool_list(post)
+		html = ""
+		pools = Pool.find(:all, :joins => "JOIN pools_posts ON pools_posts.pool_id = pools.id", :conditions => "pools_posts.post_id = #{post.id}", :order => "pools.name", :select => "pools.*")
+		
+		if pools.empty?
+			html << "none"
+		else
+			html << pools.map {|p| link_to(p.name, :controller => "pool", :action => "show", :id => p.id)}.join(", ")
+		end
+		
+		return html
+	end
+
 	def link_to_amb_tags(tags)
 		html = "The following tags are potentially ambiguous: "
 		tags = tags.map do |t|
