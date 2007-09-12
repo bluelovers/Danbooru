70502a8910d27d8962d29c841b499975166c611e
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-09-12 18:47:24 +0000

    * Fixed migration
    git-svn-id: svn://donmai.us/danbooru/trunk@590 9af9e794-c952-11db-92b5-8e906a41d509

--- db/migrate/038_favorites_add_created_at.rb
+++ db/migrate/038_favorites_add_created_at.rb
@@ -1,6 +1,7 @@
 class FavoritesAddCreatedAt < ActiveRecord::Migration
   def self.up
-    execute "alter table favorites add column created_at timestamp not null default '1960-01-01'"
+    execute "alter table favorites add column created_at timestamp not null default now()"
+    execute "update favorites set created_at = (select created_at from posts where id = favorites.post_id)"
   end
 
   def self.down
