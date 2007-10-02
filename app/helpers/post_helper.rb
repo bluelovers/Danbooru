51188a0fa9e632a2ed989991de80a65e668b6eeb
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-10-02 23:20:00 +0000

    * Implements #214: Highlight pending posts
    git-svn-id: svn://donmai.us/danbooru/trunk@696 9af9e794-c952-11db-92b5-8e906a41d509

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -7,8 +7,12 @@ module PostHelper
     if hide_unsafe_posts? && post.rating != "s"
       return ""
     end
+    
+    image_class = "preview"
+    image_class += " pending" if post.is_pending?
+    image_class += " flagged" if post.is_flagged?
 
-    image = image_tag(post.preview_url, :alt => post.cached_tags, :class => "preview", :title => post.cached_tags, :id => options[:image_id])
+    image = image_tag(post.preview_url, :alt => post.cached_tags, :class => image_class, :title => post.cached_tags, :id => options[:image_id])
     link = link_to(image, {:controller => "post", :action => "show", :id => post.id, :tag_title => post.tag_title}, :onclick => options[:onclick])
     span = content_tag "span", link, :class => "thumb", :id => "p#{post.id}"
     return span
