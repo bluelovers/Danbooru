73ebd859737b87bf8fa4171233cc769dc3513195
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-10-03 01:16:07 +0000

    * Alternative behavior for amb tags (no longer relies on wiki)
    git-svn-id: svn://donmai.us/danbooru/trunk@699 9af9e794-c952-11db-92b5-8e906a41d509

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -21,7 +21,7 @@ module PostHelper
   def link_to_amb_tags(tags)
     html = "The following tags are potentially ambiguous: "
     tags = tags.map do |t|
-      link_to(t, :controller => "wiki", :action => "show", :title => t)
+      link_to(t, :controller => "post", :action => "index", :tags => "*#{t}*")
     end
     html + tags.join(", ")
   end
