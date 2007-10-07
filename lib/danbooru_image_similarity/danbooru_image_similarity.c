b672959a432b8939b756d72c76099c9df4276b5f
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-10-07 00:17:32 +0000

    * Added confirmation to tag/mass_edit * More code for lib/danbooru_image_similarity
    git-svn-id: svn://donmai.us/danbooru/trunk@726 9af9e794-c952-11db-92b5-8e906a41d509

--- lib/danbooru_image_similarity/danbooru_image_similarity.c
+++ lib/danbooru_image_similarity/danbooru_image_similarity.c
@@ -1,33 +1,169 @@
-#include <gd.h>
+/*#include <gd.h>*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-#define SQRT_OF_TWO 1.4142135623731
+/* This is a naive implementation of the fast multiresolution image
+ * querying algorithm explained by Jacobs, Finkelstein, and Salesin
+ * in their paper of the same name.
+ */
 
-void decompose_array(float * array, int size) {
-  int i;
-  for (i=0; i<size; ++i) {
-    array[i] = array[i] / SQRT_OF_TWO;
-  }
+#define DANBOORU_SQRT2 1.4142135623731
+#define DANBOORU_M 8
+
+struct danbooru_matrix {
+  int len;
+  float * data;
+};
+
+struct danbooru_matrix * danbooru_matrix_create() {
+  struct danbooru_matrix * m = malloc(sizeof(struct danbooru_matrix));
+  m->len = 0;
+  m->data = NULL;
+  return m;
 }
 
-int main() {
-  gdImagePtr img = NULL;
-  FILE * file = fopen("test.jpg", "rb");
-	img = gdImageCreateFromGif(file);
-  int c = 0;
-  int x = 0;
+void danbooru_matrix_destroy(struct danbooru_matrix * m) {
+  free(m->data);
+  m->len = 0;
+  m->data = NULL;
+}
+
+/* PRE:
+ * 1) <a> points to an array of floating point numbers. Each number should be
+ *    in the range of [0, 1].
+ * 2) <size> is a power of 2.
+ *
+ * POST:
+ * 1) <a> will be decomposed.
+ */
+void danbooru_array_decompose(float * a, int size) {
+  int i;
   
-  for (x=0; x<gdImageSX(img); ++x) {
-    c = gdImageGetPixel(img, x, 0);
+  for (i=0; i<size; ++i) {
+    a[i] = a[i] / DANBOORU_SQRT2;
+  }
+  
+  float * ap = malloc(sizeof(float) * size);
+  
+  while (size > 1) {
+    size = size / 2;
     
-    if (gdImageGreen(img, c) > 200) {
-      printf("%d\n", gdImageGreen(img, c));
+    for (i=0; i<=size-1; ++i) {
+      ap[i] = (a[2 * i] + a[1 + (2 * i)]) / DANBOORU_SQRT2;
+      ap[size + i] = (a[2 * i] - a[1 + (2 * i)]) / DANBOORU_SQRT2;
+    }
+    
+    for (i=0; i<size; ++i) {
+      a[i] = ap[i];
     }
   }
   
-  fclose(file);
+  free(ap);
+}
+
+/* PRE:
+ * 1) <a> points to an array of floating point numbers. This array is a linear 
+ *    representation of a square matrix (that is, a matrix with the same number 
+ *    of rows as columns).
+ * 2) <n> is the number of rows or columns of <a>.
+ *
+ * POST:
+ * 1) <a> will be transposed.
+ */
+void danbooru_matrix_transpose(float * a, int n) {
+  int size = n * n;
+  float * ap = malloc(sizeof(float) * size);
+  int i, x, y;
+  
+  for (y=0; y<n; ++y) {
+    for (x=0; x<n; ++x) {
+      ap[(x * n) + y] = a[(y * n) + x];
+    }
+  }
+  
+  for (i=0; i<size; ++i) {
+    a[i] = ap[i];
+  }
+  
+  free(ap);
+}
+
+/* PRE:
+ * 1) <a> points to an array of floating point numbers. Each number is in the
+ *    range [0, 1]. This array is a linear representation of a square matrix.
+ * 2) <n> is the number of rows or columns of <a>.
+ *
+ * POST:
+ * 1) <a> will be decomposed.
+ */
+void danbooru_matrix_decompose(float * a, int n) {
+  int i;
+  float * ap;
+  
+  for (i=0; i<n; ++i) {
+    ap = a + (n * i);
+    danbooru_array_decompose(ap, n);
+  }
+  
+  danbooru_matrix_transpose(a, n);
+  
+  for (i=0; i<n; ++i) {
+    ap = a + (n * i);
+    danbooru_array_decompose(ap, n);
+  }
+  
+  danbooru_matrix_transpose(a, n);
+}
+
+struct danbooru_matrix * danbooru_matrix_normalize(int * a, int width, int height) {
+  struct danbooru_matrix * m = danbooru_matrix_create();
+  int max = (width > height) ? width : height;
+
+  m->len = 1;
+  
+  while (m->len < max) {
+    m->len = m->len * 2;
+  }
+  
+  m->data = malloc(sizeof(float) * m->len * m->len);
+  int x, y;
+  
+  for (y=0; y<m->len; ++y) {
+    for (x=0; x<m->len; ++x) {
+      if (x >= width || y >= height) {
+        m->data[(y * m->len) + x] = 0;
+      } else {
+        m->data[(y * m->len) + x] = (float)(a[(y * height) + x]) / 255.0;
+      }
+    }
+  }
+  
+  return m;
+}
+
+float * danbooru_matrix_find_largest_positive_coefficients(struct danbooru_matrix * m) {
+  return NULL;
+}
+
+float * danbooru_matrix_find_largest_negative_coefficients(struct danbooru_matrix * m) {
+  return NULL;
+}
+
+int main() {
+  int i;
+  int size = 9;
+  int x[] = {0, 128, 255, 0, 128, 255, 255, 100, 120};
+
+  struct danbooru_matrix * z = danbooru_matrix_normalize(x, 3, 3);
+  printf("n=%d\n", z->len);
+
+  for (i=0; i<16; i+=4) {
+    printf("%f %f %f %f\n", z->data[i], z->data[i+1], z->data[i+2], z->data[i+3]);
+  }
+  
+  danbooru_matrix_destroy(z);
+  free(z);
   
   return 0;
 }
---
+++ lib/danbooru_image_similarity/heap.c
@@ -0,0 +1,277 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* gheap.c - implementation of fixed-size and dynamic heap data structures
+ * Copyright (C) 1999 Jaka Mocnik
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <config.h>
+
+#include "gheap.h"
+
+#define MAX_LEVELS 64
+#define INITIAL_LEVELS 5
+
+GHeap *g_heap_new(guint size, GCompareFunc cf)
+{
+	GHeap *h;
+
+	g_return_val_if_fail(cf != NULL, NULL);
+
+	h = (GHeap *)g_new0(GHeap, 1);
+
+	h->compare = cf;
+	h->size = size;
+	h->last_index = 1;
+
+	h->heap = g_malloc(sizeof(gpointer)*size);
+
+	return h;
+}
+
+void g_heap_destroy(GHeap *h)
+{
+	g_return_if_fail(h != NULL);
+
+	if(h->heap)
+		g_free(h->heap);
+}
+
+gboolean g_heap_insert(GHeap *h, gpointer el)
+{
+	guint idx, p_idx;
+
+	g_return_val_if_fail(h != NULL, FALSE);
+	g_return_val_if_fail(el != NULL, FALSE);
+
+	idx = h->last_index;
+
+	/* insert as the lowest, right-most one */
+	if(idx > h->size)
+		return FALSE;
+	
+	h->heap[idx-1] = el;
+	h->last_index++;
+
+	/* sift the element up */
+	p_idx = idx/2;
+	while(p_idx > 0 && h->compare(el, h->heap[p_idx-1]) < 0) {
+		h->heap[idx-1] = h->heap[p_idx-1];
+		h->heap[p_idx-1] = el;
+		idx = p_idx;
+		p_idx /= 2;
+	}
+
+	return TRUE;
+}
+
+gpointer g_heap_remove(GHeap *h)
+{
+	gpointer removed, root;
+	guint idx, c_idx;
+	gboolean sifted;
+	
+	g_return_val_if_fail(h != NULL, NULL);
+	
+	if(h->last_index == 1)
+		return NULL;
+	
+	removed = h->heap[0];
+	
+	h->last_index--;
+	
+	/* put the lowest, right-most element in root */
+	root = h->heap[0] = h->heap[h->last_index-1];
+
+	/* sift it down */
+	idx = 1;
+
+	do {
+		sifted = FALSE;
+
+		/* choose the lesser child */
+		c_idx = 2*idx;
+
+		if((c_idx + 1 < h->last_index) &&
+		   (h->compare(h->heap[c_idx-1], h->heap[c_idx]) > 0))
+			c_idx++;
+
+		/* sift it down the lesser child's branch? */
+		if((c_idx < h->last_index) &&
+		   (h->compare(root, h->heap[c_idx-1]) > 0)) {
+			h->heap[idx-1] = h->heap[c_idx-1];
+			h->heap[c_idx-1] = root;
+			idx = c_idx;
+			sifted = TRUE;
+		}
+	} while(sifted);
+
+	return removed;
+}
+
+GDynamicHeap *g_dynamic_heap_new(guint max_levels, GCompareFunc cf)
+{
+	guint i, space;
+	guint initial_levels, initial_space;
+	GDynamicHeap *dh;
+
+	g_return_val_if_fail(cf != NULL, NULL);
+	g_return_val_if_fail(max_levels <= MAX_LEVELS, NULL);
+
+	dh = g_new0(GDynamicHeap, 1);
+
+	dh->compare = cf;
+
+	dh->level = g_malloc(sizeof(gpointer *)*max_levels);
+	dh->level_size = g_new0(guint64, max_levels);
+	dh->max_levels = max_levels;
+
+	dh->level_size[0] = 1;
+	for(i = 1; i < dh->max_levels; i++)
+		dh->level_size[i] = 2*dh->level_size[i - 1];
+
+	initial_levels = MIN(INITIAL_LEVELS, max_levels);
+	initial_space = 0;
+	space = 1;
+	for(i = 0; i < initial_levels; i++) {
+		initial_space += space;
+		space *= 2;
+	}
+
+	dh->level[0] = g_malloc(sizeof(gpointer *)*initial_space);
+	for(i = 1; i < initial_levels; i++)
+		dh->level[i] = dh->level[i-1] + dh->level_size[i-1];
+	for(; i < max_levels; i++)
+		dh->level[i] = NULL;
+
+	dh->last_level = 0;
+
+	return dh;
+}
+
+void g_dynamic_heap_destroy(GDynamicHeap *dh)
+{
+	gint i;
+
+	g_return_if_fail(dh != NULL);
+
+	g_free(dh->level[0]);
+
+	for(i = MIN(INITIAL_LEVELS, dh->max_levels);
+		i < dh->max_levels && dh->level[i] != NULL;
+		i++)
+		g_free(dh->level[i]);
+
+	g_free(dh->level);
+	g_free(dh->level_size);
+}
+
+void g_dynamic_heap_insert(GDynamicHeap *dh, gpointer el)
+{
+	guint idx, lev, p_idx, p_lev;
+
+	g_return_if_fail(dh != NULL);
+	g_return_if_fail(el != NULL);
+	g_return_if_fail(dh->last_level < dh->max_levels);
+
+	idx = dh->last_index;
+	lev = dh->last_level;
+
+	/* allocate a new level if necessary */
+	if(idx == 0 && dh->level[lev] == NULL)
+		dh->level[lev] = g_malloc(sizeof(gpointer)*dh->level_size[lev]);
+
+	/* insert as the lowest, right-most one */
+	dh->level[lev][idx] = el;
+
+	dh->last_index++;
+	if(dh->last_index >= dh->level_size[dh->last_level]) {
+		dh->last_index = 0;
+		dh->last_level++;
+	}
+
+	/* sift the element up */
+	p_lev = lev - 1;
+	p_idx = idx/2;
+	while(lev > 0 && (dh->compare(el, dh->level[p_lev][p_idx]) < 0)) {
+		dh->level[lev][idx] = dh->level[p_lev][p_idx];
+		dh->level[p_lev][p_idx] = el;
+		lev = p_lev;
+		idx = p_idx;
+		p_lev--;
+		p_idx /= 2;
+	}
+}
+
+gpointer g_dynamic_heap_remove(GDynamicHeap *dh)
+{
+	gpointer removed, root;
+	guint idx, lev, c_idx, c_lev;
+	gboolean sifted;
+
+	g_return_val_if_fail(dh != NULL, NULL);
+
+	if(dh->last_level == 0 && dh->last_index == 0)
+		return NULL;
+
+	removed = dh->level[0][0];
+
+	/* decrease the last_level and last_index */
+	if(dh->last_index == 0) {
+		if(dh->level[dh->last_level + 1] != NULL &&
+		   dh->last_level + 1 >= MIN(INITIAL_LEVELS, dh->max_levels))
+			g_free(dh->level[dh->last_level + 1]);
+		dh->last_level--;
+		dh->last_index = dh->level_size[dh->last_level];
+	}
+	else
+		dh->last_index--;
+
+	if(dh->last_level == 0 && dh->last_index == 0)
+		return removed;
+
+	/* put the lowest, right-most element in root */
+	root = dh->level[0][0] = dh->level[dh->last_level][dh->last_index - 1];
+
+	/* sift it down */
+	idx = lev = 0;
+
+	do {
+		sifted = FALSE;
+
+		/* choose the lesser child */
+		c_idx = 2*idx;
+		c_lev = lev + 1;
+
+		if((c_lev < dh->last_level ||
+			(c_lev == dh->last_level && c_idx + 1 < dh->last_index)) &&
+		   (dh->compare(dh->level[c_lev][c_idx], dh->level[c_lev][c_idx + 1]) > 0))
+			c_idx++;
+
+		/* sift it down the lesser child's branch? */
+		if((c_lev < dh->last_level ||
+			(c_lev == dh->last_level && c_idx < dh->last_index)) &&
+		   (dh->compare(root, dh->level[c_lev][c_idx]) > 0)) {
+			dh->level[lev][idx] = dh->level[c_lev][c_idx];
+			dh->level[c_lev][c_idx] = root;
+			idx = c_idx;
+			lev = c_lev;
+			sifted = TRUE;
+		}
+	} while(sifted);
+
+	return removed;
+}
