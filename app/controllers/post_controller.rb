163d7557cec6865b13b53b0a07349123077db4c3
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-10-29 00:53:14 +0000

    * Fixed ordering on post/moderate
    git-svn-id: svn://donmai.us/danbooru/trunk@857 9af9e794-c952-11db-92b5-8e906a41d509

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -93,7 +93,7 @@ class PostController < ApplicationController
       if params[:query]
         @posts = Post.find_by_sql(Post.generate_sql(params[:query], :pending => true, :order => "id desc"))
       else
-        @posts = Post.find(:all, :conditions => "id in (select post_id from flagged_posts where is_resolved = false) OR is_pending = TRUE", :order => "id")
+        @posts = Post.find(:all, :conditions => "id in (select post_id from flagged_posts where is_resolved = false) OR is_pending = TRUE", :order => "id desc")
       end
     end
   end
