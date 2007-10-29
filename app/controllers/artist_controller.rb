d2348f8c8a04172bd8bcc7cf318d2320f9fe23e8
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-10-29 19:43:25 +0000

    * Artist search is now case insensitive
    git-svn-id: svn://donmai.us/danbooru/trunk@868 9af9e794-c952-11db-92b5-8e906a41d509

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -88,7 +88,7 @@ class ArtistController < ApplicationController
       elsif params[:name] =~ /^[a-fA-F0-9]{32,32}$/
         @artists = Artist.find_all_by_md5(params[:name])
       else
-        @pages, @artists = paginate :artists, :conditions => ["name LIKE ? ESCAPE '\\\\'", '%' + params[:name].to_escaped_for_sql_like + '%'], :order => "name", :per_page => 50
+        @pages, @artists = paginate :artists, :conditions => ["name ILIKE ? ESCAPE '\\\\'", '%' + params[:name].to_escaped_for_sql_like + '%'], :order => "name", :per_page => 50
       end
     elsif params[:url]
       @artists = Artist.find_all_by_url(params[:url])
