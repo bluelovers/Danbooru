72b6855d9127f78f5c01a1b68c7e488e522b40da
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-10-29 19:42:36 +0000

    * Potential fix for #245: Forum threads prematurely marked as read
    git-svn-id: svn://donmai.us/danbooru/trunk@867 9af9e794-c952-11db-92b5-8e906a41d509

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -85,7 +85,7 @@ class ForumController < ApplicationController
     @forum_post = ForumPost.find(params[:id])
     @pages, @children = paginate :forum_posts, :order => "id", :per_page => 10, :conditions => ["parent_id = ?", params[:id]]
 
-    if @current_user != nil && @current_user.last_forum_topic_read_at < @forum_post.updated_at && @forum_post.updated_at < 3.seconds.ago
+    if @current_user != nil && @current_user.last_forum_topic_read_at < @forum_post.updated_at && @forum_post.updated_at < 6.seconds.ago
       @current_user.update_attribute(:last_forum_topic_read_at, @forum_post.updated_at)
     end
     
