e4785233a59c2b843c85cb4cb7cc6cda45abf17f
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-11-02 15:13:58 +0000

    * Added two missing files * Fixed routes to handle dashes in ids (needed for note/update)
    git-svn-id: svn://donmai.us/danbooru/trunk@884 9af9e794-c952-11db-92b5-8e906a41d509

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect "", :controller => "static", :action => "index"
   map.connect "post/show/:id/:tag_title", :controller => "post", :action => "show", :requirements => {:id => /\d+/}
-  map.connect ":controller/:action/:id.:format", :requirements => {:id => /\d+/}
-  map.connect ":controller/:action/:id", :requirements => {:id => /\d+/}
+  map.connect ":controller/:action/:id.:format", :requirements => {:id => /[-\d]+/}
+  map.connect ":controller/:action/:id", :requirements => {:id => /[-\d]+/}
   map.connect ":controller/:action.:format"
   map.connect ":controller/:action"
 end
