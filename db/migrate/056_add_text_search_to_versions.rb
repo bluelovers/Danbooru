abfb5b4554bf357b0672051ef256cedae903aca9
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-11-10 01:55:58 +0000

    * Fixed migrations
    git-svn-id: svn://donmai.us/danbooru/trunk@911 9af9e794-c952-11db-92b5-8e906a41d509

---
+++ db/migrate/056_add_text_search_to_versions.rb
@@ -0,0 +1,9 @@
+class AddTextSearchToVersions < ActiveRecord::Migration
+  def self.up
+    execute "alter table note_versions add column text_search_index tsvector"
+    execute "alter table wiki_page_versions add column text_search_index tsvector"
+  end
+
+  def self.down
+  end
+end
