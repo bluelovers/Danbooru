8ba806bf0f2a5bd2ac4f71f6050aa4450c85083c
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-11-13 07:38:53 +0000

    * Added admin/reset_password
    git-svn-id: svn://donmai.us/danbooru/trunk@916 9af9e794-c952-11db-92b5-8e906a41d509

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -21,6 +21,30 @@ class AdminController < ApplicationController
     end
   end
 
+  def reset_password
+    if request.post?
+      @user = User.find_by_name(params[:user][:name])
+      
+      if @user
+        if @user.email.blank?
+          flash[:notice] = "User never supplied an email address"
+          redirect_to :action => "index"
+        else
+          new_password = @user.reset_password
+          UserMailer.deliver_new_password(@user, new_password)
+
+          flash[:notice] = "Password reset"
+          redirect_to :action => "index"
+        end
+      else
+        flash[:notice] = "That account does not exist"
+        redirect_to :action => "reset_password"
+      end
+    else
+      @user = User.new
+    end
+  end
+
   def reset_cache
     $cache_version += 1
     redirect_to :action => "index"
