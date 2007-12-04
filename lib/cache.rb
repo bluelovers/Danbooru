973500019c30f7bdec5ac3e10d61a2a2ac1a29a1
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2007-12-04 19:56:04 +0000

    * Fixed bug where $cache_version might get out of sync between multiple processes * Fixed bug in cache expiration
    git-svn-id: svn://donmai.us/danbooru/trunk@1019 9af9e794-c952-11db-92b5-8e906a41d509

--- lib/cache.rb
+++ lib/cache.rb
@@ -10,7 +10,9 @@ module Cache
       end
   
       $cache_version = CACHE.incr("$cache_version")
-    elsif options[:post_id]
+    end
+    
+    if options[:post_id]
       CACHE.set("p/s/#{options[:post_id]}", nil)
     end
   end
