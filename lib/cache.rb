3cdc9d393718ffbe6ffe4b761990e9a9569bd2f4
danbooru <danbooru@9af9e794-c952-11db-92b5-8e906a41d509>
2007-12-09 16:29:16 +0000

    * Activated post/show caching * Bug fix with user/index
    git-svn-id: svn://donmai.us/danbooru/trunk@1047 9af9e794-c952-11db-92b5-8e906a41d509

--- lib/cache.rb
+++ lib/cache.rb
@@ -1,14 +1,18 @@
 module Cache
   def expire(options = {})
-    cache_version = Cache.get("$cache_version") {0}
-    Cache.put("$cache_version", cache_version + 1)
-    
     if options[:tags]
       options[:tags].scan(/\S+/).each do |x|
         key = "tag:#{x}"
         key_version = Cache.get(key) {0}
         Cache.put(key, key_version + 1)
       end
+    end
+
+    if options[:post_id]
+      Cache.put("p/s/#{options[:post_id]}", nil)
+    else  
+      cache_version = Cache.get("$cache_version") {0}
+      Cache.put("$cache_version", cache_version + 1)
     end
   end
   
