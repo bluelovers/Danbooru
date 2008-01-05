9a1ee983d63792e6c05adf907534b97b0dd3d2d7
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-01-05 22:49:52 +0000

    * Added LocalFlatWithAmazonS3Backup image store option
    git-svn-id: svn://donmai.us/danbooru/trunk@1139 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/post.rb
+++ app/models/post.rb
@@ -376,6 +376,9 @@ class Post < ActiveRecord::Base
       case type
       when :local_flat
         include LocalFlat
+        
+      when :local_flat_with_amazon_s3_backup
+        include LocalFlatWithAmazonS3Backup
 
       when :local_hierarchy
         include LocalHierarchy
@@ -423,8 +426,6 @@ class Post < ActiveRecord::Base
         FileUtils.chmod(0775, file_path)
 
         if image?
-          puts tempfile_preview_path
-          puts preview_path
           FileUtils.mv(tempfile_preview_path, preview_path)
           FileUtils.chmod(0775, preview_path)
         end
@@ -552,19 +553,16 @@ class Post < ActiveRecord::Base
         begin
           base64_md5 = Base64.encode64(self.md5.unpack("a2" * (self.md5.size / 2)).map {|x| x.hex.chr}.join)
           
-          Timeout::timeout(5) do
-            AWS::S3::Base.establish_connection!(:access_key_id => CONFIG["amazon_s3_access_key_id"], :secret_access_key => CONFIG["amazon_s3_secret_access_key"])
-            AWS::S3::S3Object.store(file_name, open(self.tempfile_path, "rb"), CONFIG["amazon_s3_bucket_name"], :access => :public_read, "Content-MD5" => base64_md5, "Cache-Control" => "max-age=315360000")
-            
-            if image?
-              AWS::S3::S3Object.store("preview/#{md5}.jpg", open(self.tempfile_preview_path, "rb"), CONFIG["amazon_s3_bucket_name"], :access => :public_read, "Cache-Control" => "max-age=315360000")
-            end
-            self.delete_tempfile()
+          AWS::S3::Base.establish_connection!(:access_key_id => CONFIG["amazon_s3_access_key_id"], :secret_access_key => CONFIG["amazon_s3_secret_access_key"])
+          AWS::S3::S3Object.store(file_name, open(self.tempfile_path, "rb"), CONFIG["amazon_s3_bucket_name"], :access => :public_read, "Content-MD5" => base64_md5, "Cache-Control" => "max-age=315360000")
+          
+          if image?
+            AWS::S3::S3Object.store("preview/#{md5}.jpg", open(self.tempfile_preview_path, "rb"), CONFIG["amazon_s3_bucket_name"], :access => :public_read, "Cache-Control" => "max-age=315360000")
           end
+
           return true
-        rescue Exception => e
+        ensure
           self.delete_tempfile()
-          raise
         end
       end
 
@@ -584,6 +582,67 @@ class Post < ActiveRecord::Base
         AWS::S3::Base.establish_connection!(:access_key_id => CONFIG["amazon_s3_access_key_id"], :secret_access_key => CONFIG["amazon_s3_secret_access_key"])
         AWS::S3::S3Object.delete(file_name, CONFIG["amazon_s3_bucket_name"])
         AWS::S3::S3Object.delete("preview/#{md5}.jpg", CONFIG["amazon_s3_bucket_name"])
+      end
+    end
+
+    module LocalFlatWithAmazonS3Backup
+      def move_file
+        FileUtils.mv(tempfile_path, file_path)
+        FileUtils.chmod(0775, file_path)
+
+        if image?
+          FileUtils.mv(tempfile_preview_path, preview_path)
+          FileUtils.chmod(0775, preview_path)
+        end
+        
+        self.delete_tempfile()
+        
+        base64_md5 = Base64.encode64(self.md5.unpack("a2" * (self.md5.size / 2)).map {|x| x.hex.chr}.join)
+        
+        AWS::S3::Base.establish_connection!(:access_key_id => CONFIG["amazon_s3_access_key_id"], :secret_access_key => CONFIG["amazon_s3_secret_access_key"])
+        AWS::S3::S3Object.store(file_name, open(self.file_path, "rb"), CONFIG["amazon_s3_bucket_name"], :access => :private, "Content-MD5" => base64_md5)
+        
+        if image?
+          AWS::S3::S3Object.store("preview/#{md5}.jpg", open(self.preview_path, "rb"), CONFIG["amazon_s3_bucket_name"], :access => :private)
+        end
+
+        return true
+      end
+
+      def file_hierarchy
+        "%s/%s" % [md5[0,2], md5[2,2]]
+      end
+
+      def file_path
+        "#{RAILS_ROOT}/public/data/#{file_name}"
+      end
+
+      def file_url
+        CONFIG["url_base"] + "/data/#{file_name}"
+      end
+
+      def preview_path
+        if image?
+          "#{RAILS_ROOT}/public/data/preview/#{md5}.jpg"
+        else
+          "#{RAILS_ROOT}/public/data/preview/default.png"
+        end
+      end
+
+      def preview_url
+        if image?
+          CONFIG["url_base"] + "/data/preview/#{md5}.jpg"
+        else
+          CONFIG["url_base"] + "/data/preview/default.png"
+        end
+      end
+
+      def delete_file
+        AWS::S3::Base.establish_connection!(:access_key_id => CONFIG["amazon_s3_access_key_id"], :secret_access_key => CONFIG["amazon_s3_secret_access_key"])
+        AWS::S3::S3Object.delete(file_name, CONFIG["amazon_s3_bucket_name"])
+        AWS::S3::S3Object.delete("preview/#{md5}.jpg", CONFIG["amazon_s3_bucket_name"])
+        FileUtils.rm_f(file_path)
+        FileUtils.rm_f(preview_path) if image?
       end
     end
   end
