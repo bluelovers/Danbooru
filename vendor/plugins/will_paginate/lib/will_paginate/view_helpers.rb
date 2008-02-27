d6d98ee7862082236ca0102af6958b4c8cb93feb
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-02-27 17:05:36 +0000

    * Fixes #139: New plugin displays an huge paginator (trunk)
    git-svn-id: svn://donmai.us/danbooru/trunk@1276 9af9e794-c952-11db-92b5-8e906a41d509

--- vendor/plugins/will_paginate/lib/will_paginate/view_helpers.rb
+++ vendor/plugins/will_paginate/lib/will_paginate/view_helpers.rb
@@ -24,8 +24,8 @@ module WillPaginate
       :class        => 'pagination',
       :prev_label   => '&lt;&lt;',
       :next_label   => '&gt;&gt;',
-      :inner_window => 4, # links around the current page
-      :outer_window => 1, # links around beginning and end
+      :inner_window => 2, # links around the current page
+      :outer_window => 0, # links around beginning and end
       :separator    => ' ', # single space is friendly to spiders and non-graphic browsers
       :param_name   => :page,
       :params       => nil,
