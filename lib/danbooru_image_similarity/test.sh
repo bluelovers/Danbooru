e8660e4c7c57aeb4b4111443762da402a6bf3133
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-03-05 20:42:50 +0000

    * Removed similarity plugin
    git-svn-id: svn://donmai.us/danbooru/trunk@1321 9af9e794-c952-11db-92b5-8e906a41d509

--- lib/danbooru_image_similarity/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#gcc -fno-common -Wall -ggdb -DHAVE_GLIB_H -DHAVE_GD_H -DHAVE_GDIMAGECREATEFROMJPEG -DHAVE_GDIMAGECREATEFROMGIF -DHAVE_GDIMAGECREATEFROMPNG -DHAVE_GDIMAGEJPEG -I/usr/local/lib/glib/include -I/usr/local/include/glib-1.2 danbooru_image_similarity.c gheap.c -lglib -lgd -o test
-
-gcc -fno-common -Wall -O2 -DHAVE_GLIB_H -DHAVE_GD_H -DHAVE_GDIMAGECREATEFROMJPEG -DHAVE_GDIMAGECREATEFROMGIF -DHAVE_GDIMAGECREATEFROMPNG -DHAVE_GDIMAGEJPEG -I/usr/local/lib/glib-2.0/include -I/usr/local/include/glib-2.0 -c danbooru_image_similarity.c -o danbooru_image_similarity.o
-gcc -fno-common -Wall -O2 -DHAVE_GLIB_H -DHAVE_GD_H -DHAVE_GDIMAGECREATEFROMJPEG -DHAVE_GDIMAGECREATEFROMGIF -DHAVE_GDIMAGECREATEFROMPNG -DHAVE_GDIMAGEJPEG -I/usr/local/lib/glib-2.0/include -I/usr/local/include/glib-2.0 -c gheap.c -o gheap.o
-gcc -L/usr/local/lib -lglib-2.0 -lgd -fno-common -Wall -ggdb gheap.o danbooru_image_similarity.o -o test
