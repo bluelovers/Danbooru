68dfb62232f59b67c1fad941ecf6d0a4da0c99ef
danbooru <danbooru@9af9e794-c952-11db-92b5-8e906a41d509>
2008-03-06 02:27:42 +0000

    * Removed references to gd
    git-svn-id: svn://donmai.us/danbooru/trunk@1322 9af9e794-c952-11db-92b5-8e906a41d509

--- lib/danbooru_image_resizer/extconf.rb
+++ lib/danbooru_image_resizer/extconf.rb
@@ -4,25 +4,19 @@ require 'mkmf'
 
 CONFIG['CC'] = "g++"
 
-dir_config("gd")
+#dir_config("gd")
 dir_config("jpeg")
 dir_config("png")
 
-ok = true
+#have_header("gd.h")
 
-ok &&= have_header("gd.h")
+#have_library("gd")
+have_library("jpeg")
+have_library("png")
 
-ok &&= have_library("gd")
-ok &&= have_library("jpeg")
-ok &&= have_library("png")
-
-ok &&= have_func("gdImageCreateFromGif", "gd.h")
-ok &&= have_func("gdImageJpeg", "gd.h")
-ok &&= have_func("jpeg_set_quality", ["stdlib.h", "stdio.h", "jpeglib-extern.h"])
-
-if !ok
-  raise "Missing prerequisites"
-end
+#have_func("gdImageCreateFromGif", "gd.h")
+#have_func("gdImageJpeg", "gd.h")
+have_func("jpeg_set_quality", ["stdlib.h", "stdio.h", "jpeglib-extern.h"])
 
 with_cflags("-O2 -fno-exceptions -Wall") {true}
 #with_cflags("-O0 -g -fno-exceptions -Wall") {true}
