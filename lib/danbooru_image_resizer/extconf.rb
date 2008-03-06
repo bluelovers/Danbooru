e65e974125ac77a30b26d4a4d579ddcb2a8f412b
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-03-06 19:11:04 +0000

    * Readded gd dependency
    git-svn-id: svn://donmai.us/danbooru/trunk@1334 9af9e794-c952-11db-92b5-8e906a41d509

--- lib/danbooru_image_resizer/extconf.rb
+++ lib/danbooru_image_resizer/extconf.rb
@@ -4,18 +4,18 @@ require 'mkmf'
 
 CONFIG['CC'] = "g++"
 
-#dir_config("gd")
+dir_config("gd")
 dir_config("jpeg")
 dir_config("png")
 
-#have_header("gd.h")
+have_header("gd.h")
 
-#have_library("gd")
+have_library("gd")
 have_library("jpeg")
 have_library("png")
 
-#have_func("gdImageCreateFromGif", "gd.h")
-#have_func("gdImageJpeg", "gd.h")
+have_func("gdImageCreateFromGif", "gd.h")
+have_func("gdImageJpeg", "gd.h")
 have_func("jpeg_set_quality", ["stdlib.h", "stdio.h", "jpeglib-extern.h"])
 
 with_cflags("-O2 -fno-exceptions -Wall") {true}
