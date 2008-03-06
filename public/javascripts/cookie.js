3d84198cebce7e3e5fe7690ca215db030f9bdadb
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-03-06 18:55:17 +0000

    * Fixes #167: public/javascripts/cookie.js hardcodes danbooru.donmai.us
    git-svn-id: svn://donmai.us/danbooru/trunk@1333 9af9e794-c952-11db-92b5-8e906a41d509

--- public/javascripts/cookie.js
+++ public/javascripts/cookie.js
@@ -42,8 +42,8 @@ Cookie = {
   },
 
   setup: function() {
-    if (location.href.match(/danbooru\.donmai\.us\/(comment|pool|note|post)/) && this.get("tos") != "1") {
-     location.href = "http://danbooru.donmai.us/static/terms_of_service?url=" + location.href
+    if (location.pathname.match(/^\/(comment|pool|note|post)/) && this.get("tos") != "1") {
+     location.pathname = "/static/terms_of_service?url=" + location.href
      return
     }
     
