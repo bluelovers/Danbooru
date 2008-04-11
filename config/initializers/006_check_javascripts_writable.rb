93c34cb496420b117afd4b9abe62188be3f7e9ab
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-04-11 17:23:06 +0000

    * Fixes #293: Initializer 006 generates errors
    git-svn-id: svn://donmai.us/danbooru/trunk@1480 9af9e794-c952-11db-92b5-8e906a41d509

--- config/initializers/006_check_javascripts_writable.rb
+++ config/initializers/006_check_javascripts_writable.rb
@@ -1,7 +1,9 @@
 if true
-  path = "public/javascripts"
+  path = ""
+  path += "#{RAILS_ROOT}/" if defined?(RAILS_ROOT)
+  path += "public/javascripts"
 
-  if not File.stat(path).writable?
+  if not File.writable?(path)
     raise "Path must be writable: %s" % path
   end
 end
