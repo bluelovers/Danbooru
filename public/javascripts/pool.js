076e0cfee3e7953094f264f70c50f82b767eabae
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-04-28 19:23:33 +0000

    * Fixes #320: allow removing from pool from post/show; tweak formatting
    git-svn-id: svn://donmai.us/danbooru/trunk@1517 9af9e794-c952-11db-92b5-8e906a41d509

--- public/javascripts/pool.js
+++ public/javascripts/pool.js
@@ -20,27 +20,24 @@ Pool = {
   },
 
   remove_post: function(post_id, pool_id) {
-    if ($("del-mode") && $("del-mode").checked == true) {
-      new Ajax.Request('/pool/remove_post.json', {
-        parameters: {
-          "post_id": post_id,
-          "pool_id": pool_id
-        },
-        onComplete: function(resp) {
-          var resp = resp.responseJSON
-          
-          if (resp.success) {
-            notice("Post removed from pool")
+    new Ajax.Request('/pool/remove_post.json', {
+      parameters: {
+        "post_id": post_id,
+        "pool_id": pool_id
+      },
+      onComplete: function(resp) {
+        var resp = resp.responseJSON
+        
+        if (resp.success) {
+          notice("Post removed from pool")
+          if($("p" + post_id))
             $("p" + post_id).remove()            
-          } else {
-            notice("Error: " + resp.reason)
-          }          
-        }
-      })
-
-      return false
-    } else {
-      return true
-    }
+          if($("pool" + pool_id))
+            $("pool" + pool_id).remove()            
+        } else {
+          notice("Error: " + resp.reason)
+        }          
+      }
+    })
   }
 }
