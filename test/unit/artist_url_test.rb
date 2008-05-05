0c5447ab8eb1de07c3b2183a38cfef53011f01c3
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-05-05 18:48:21 +0000

    * Added artist and artist_url unit tests
    git-svn-id: svn://donmai.us/danbooru/trunk@1544 9af9e794-c952-11db-92b5-8e906a41d509

---
+++ test/unit/artist_url_test.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ArtistUrlTest < ActiveSupport::TestCase
+  fixtures :users, :artists
+  
+  def test_normalize
+    url = ArtistUrl.create(:artist_id => 1, :url => nil)
+    assert !url.valid?
+    
+    url = ArtistUrl.create(:artist_id => 1, :url => "http://monet.com")
+    assert_equal("http://monet.com", url.url)
+    assert_equal("http://monet.com", url.normalized_url)
+
+    url = ArtistUrl.create(:artist_id => 1, :url => "http://monet.com/")
+    assert_equal("http://monet.com/", url.url)
+    assert_equal("http://monet.com", url.normalized_url)
+
+    url = ArtistUrl.create(:artist_id => 1, :url => "http://blog55.fc2.com/monet")
+    assert_equal("http://blog55.fc2.com/monet", url.url)
+    assert_equal("http://blog.fc2.com/monet", url.normalized_url)
+
+    url = ArtistUrl.create(:artist_id => 1, :url => "http://blog-imgs-55.fc2.com/monet")
+    assert_equal("http://blog-imgs-55.fc2.com/monet", url.url)
+    assert_equal("http://blog.fc2.com/monet", url.normalized_url)
+
+    url = ArtistUrl.create(:artist_id => 1, :url => "http://img55.pixiv.net/monet")
+    assert_equal("http://img55.pixiv.net/monet", url.url)
+    assert_equal("http://img.pixiv.net/monet", url.normalized_url)
+  end
+end
