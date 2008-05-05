629e4c11b6ed446797c51598b044a7f748862c07
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-05-05 19:17:03 +0000

    * Creating a ban now updates the user's level
    git-svn-id: svn://donmai.us/danbooru/trunk@1546 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/ban.rb
+++ app/models/ban.rb
@@ -1,5 +1,10 @@
 class Ban < ActiveRecord::Base
   after_create :save_to_record
+  before_create :update_level
+  
+  def update_level
+    User.update(user_id, :level => CONFIG["user_levels"]["Blocked"])
+  end
   
   def save_to_record
     UserRecord.create(:user_id => self.user_id, :reported_by => self.banned_by, :is_positive => false, :body => "Blocked: #{self.reason}")
