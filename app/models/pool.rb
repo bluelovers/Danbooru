3374c387f6167e0a3956ff2d1422df0c6091b8b6
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-05-06 22:58:17 +0000

    * Refactored pool model * Fixed bug where Pool#update_pool_links was using stale pool_posts * Fixd bug where pool name was being normalized after the uniqueness check, leading to potential duplicates
    git-svn-id: svn://donmai.us/danbooru/trunk@1552 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,101 +1,120 @@
-class Pool < ActiveRecord::Base
-  class PostAlreadyExistsError < Exception; end
-  
+class Pool < ActiveRecord::Base  
   belongs_to :user
-  validates_uniqueness_of :name
-  before_save :normalize_name
-  has_many :pool_posts, :class_name => "PoolPost", :order => "sequence"
   
-  def self.find_by_name(name)
-    if name =~ /^\d+$/
-      find_by_id(name)
-    else
-      find(:first, :conditions => ["lower(name) = lower(?)", name])
+  class PostAlreadyExistsError < Exception
+  end
+  
+  module PostMethods    
+    def self.included(m)
+      m.has_many :pool_posts, :class_name => "PoolPost", :order => "sequence"
     end
-  end
-  
-  def normalize_name
-    self.name = self.name.gsub(/\s/, "_")
-  end
-  
-  def pretty_name
-    self.name.gsub(/_/, " ")
-  end
-  
-  def add_post(post_id, options = {})
-    transaction do
-      if PoolPost.find(:first, :conditions => ["pool_id = ? and post_id = ?", self.id, post_id])
-        raise PostAlreadyExistsError
-      end
-      seq = options.fetch(:sequence, next_id)
-      Cache.expire(:post_id => post_id)
-      update_attributes(:updated_at => Time.now)
-      PoolPost.create(:pool_id => self.id, :post_id => post_id, :sequence => seq.to_i)
-      self.increment(:post_count)
-      self.save!
+    
+    def add_post(post_id, options = {})
+      transaction do
+        if PoolPost.exists?(["pool_id = ? AND post_id = ?", id, post_id])
+          raise PostAlreadyExistsError
+        end
+        
+        seq = options[:sequence] || next_sequence
+        PoolPost.create(:pool_id => id, :post_id => post_id, :sequence => seq.to_i)
+        increment!(:post_count)
 
-      if !options.fetch(:skip_update_pool_links, false)
-        self.update_pool_links
+        unless options[:skip_update_pool_links]
+          update_pool_links
+        end
       end
     end
-  end
-  
-  def remove_post(post_id)
-    transaction do
-      return unless PoolPost.find(:first, :conditions => ["pool_id = ? and post_id = ?", self.id, post_id])
 
-      Cache.expire(:post_id => post_id)
-      update_attributes(:updated_at => Time.now)
-      PoolPost.destroy_all(["pool_id = ? and post_id = ?", self.id, post_id])
-      self.decrement(:post_count)
-      self.save!
-
-      self.update_pool_links
+    def remove_post(post_id)
+      transaction do
+        if PoolPost.exists?(["pool_id = ? and post_id = ?", id, post_id])
+          PoolPost.destroy_all(["pool_id = ? and post_id = ?", id, post_id])
+          decrement!(:post_count)
+          update_pool_links
+        end
+      end
     end
-  end
+    
+    def update_pool_links
+      transaction do
+        pp = pool_posts(true) # force reload
+        pp.each_index do |i|
+          pp[i].next_post_id = nil
+          pp[i].prev_post_id = nil
+          pp[i].next_post_id = pp[i + 1].post_id unless i == pp.size - 1
+          pp[i].prev_post_id = pp[i - 1].post_id unless i == 0
+          pp[i].save
+        end
+      end
+    end
 
-  def update_pool_links
-    transaction do
-      pp = self.pool_posts
-      pp.each_index do |i|
-        pp[i].next_post_id = nil
-        pp[i].prev_post_id = nil
-        pp[i].next_post_id = pp[i + 1].post_id unless i == pp.size - 1
-        pp[i].prev_post_id = pp[i - 1].post_id unless i == 0
-        pp[i].save
+    def next_sequence
+      seq = select_value_sql("SELECT MAX(sequence) FROM pools_posts where pool_id = ?", id)
+      
+      if seq.nil?
+        return 0
+      else
+        return seq.to_i + 1
       end
     end
   end
+  
+  module ApiMethods
+    def api_attributes
+      return {
+        :id => id,
+        :name => name,
+        :created_at => created_at,
+        :updated_at => updated_at,
+        :user_id => user_id,
+        :is_public => is_public,
+        :post_count => post_count,
+      }
+    end
+    
+    def to_json(*params)
+      api_attributes.to_json(*params)
+    end
 
-  def next_id()
-    i = connection.select_value("SELECT MAX(sequence) FROM pools_posts where pool_id=#{self.id}")
-    if i.nil?
-      return 0
-    else
-      return i.to_i + 1
+    def to_xml(options = {})
+      options[:indent] ||= 2
+      xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
+      xml.pool(api_attributes) do
+        xml.description(description)
+        yield options[:builder] if block_given?
+      end
     end
   end
+  
+  module NameMethods
+    module ClassMethods
+      def find_by_name(name)
+        if name =~ /^\d+$/
+          find_by_id(name)
+        else
+          find(:first, :conditions => ["lower(name) = lower(?)", name])
+        end
+      end
+    end
+    
+    def self.included(m)
+      m.extend(ClassMethods)
+      m.validates_uniqueness_of :name
+      m.before_validation :normalize_name
+    end
+    
+    def normalize_name
+      self.name = name.gsub(/\s/, "_")
+    end
 
-  def api_attributes
-    return {
-      :id => id,
-      :name => name,
-      :created_at => created_at,
-      :updated_at => updated_at,
-      :user_id => user_id,
-      :is_public => is_public,
-      :post_count => post_count,
-    }
-  end
-
-  def to_xml(options = {})
-    options[:indent] ||= 2
-    xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
-    xml.pool(api_attributes) do
-      xml.description(description)
-      yield options[:builder]
+    def pretty_name
+      name.tr("_", " ")
     end
   end
+  
+  include PostMethods
+  include ApiMethods
+  include NameMethods
 end
 
 class PoolPost < ActiveRecord::Base
