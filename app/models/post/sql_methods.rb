9105fc6a8cfa0ffa03a25582af79c0f629179719
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-05-19 16:21:53 +0000

    * Fixed bug with source searches being double escaped
    git-svn-id: svn://donmai.us/danbooru/trunk@1565 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/post/sql_methods.rb
+++ app/models/post/sql_methods.rb
@@ -71,7 +71,7 @@ module PostSqlMethods
 
       if q[:source].is_a?(String)
         conds << "p.source LIKE ? ESCAPE E'\\\\'"
-        cond_params << q[:source].to_escaped_for_sql_like
+        cond_params << q[:source]
       end
 
       if q[:fav].is_a?(String)
