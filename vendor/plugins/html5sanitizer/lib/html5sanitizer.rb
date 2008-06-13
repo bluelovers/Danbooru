ab98e4a9525b642d72b2426f5bec79fd1a266a1b
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-06-13 15:27:04 +0000

    * Fixes #349: html5sanitizer serializes as xhtml
    git-svn-id: svn://donmai.us/danbooru/trunk@1622 9af9e794-c952-11db-92b5-8e906a41d509

--- vendor/plugins/html5sanitizer/lib/html5sanitizer.rb
+++ vendor/plugins/html5sanitizer/lib/html5sanitizer.rb
@@ -1,13 +1,19 @@
 module HTML5Sanitizer
 	require 'html5'
 	require 'html5/sanitizer'
+	require 'html5/treewalkers'
+	require 'html5/serializer'
 	include HTML5
 	
 	def html5sanitize(html_fragment)
 		old_kcode=$KCODE
 		$KCODE="NONE"
-		s = HTMLParser.parse_fragment(html_fragment, {:tokenizer => HTMLSanitizer, :encoding => 'utf-8'}).to_s
+		trees = HTMLParser.parse_fragment(html_fragment, {:tokenizer => HTMLSanitizer, :encoding => 'utf-8'})
 		$KCODE=old_kcode
+		s = ""
+		for t in trees do
+			s += HTMLSerializer.serialize(TreeWalkers.get_tree_walker('rexml').new(t))
+		end
 		return s
 	end
 	
