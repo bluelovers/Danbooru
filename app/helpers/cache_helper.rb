2f085f718092d081a90d2dfbb4028ff0a7e0e26b
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-06-16 16:05:59 +0000

    * Don't cache tag queries with negated tags
    git-svn-id: svn://donmai.us/danbooru/trunk@1628 9af9e794-c952-11db-92b5-8e906a41d509

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -2,7 +2,7 @@ module CacheHelper
   def build_cache_key(base, tags, page, limit, options = {})
     page = page.to_i
     page = 1 if page < 1
-    tags = tags.to_s.downcase.scan(/\S+/).map { |s| s.gsub(/^-/, "") }. sort
+    tags = tags.to_s.downcase.scan(/\S+/).sort
     
     if options[:user]
       user_level = options[:user].level
@@ -11,7 +11,7 @@ module CacheHelper
       user_level = "?"
     end
     
-    if tags.empty? || tags.any? {|x| x =~ /[*:]/}
+    if tags.empty? || tags.any? {|x| x =~ /(?:^-|[*:])/}
       version = Cache.get("$cache_version").to_i
       tags = tags.join(",")
     else
