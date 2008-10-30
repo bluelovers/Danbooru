f3795b53bf4ed30c94f778cb26efee4d40e74590
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-10-30 21:54:18 +0000

    * Fixes #421: Resize images is useless
    git-svn-id: svn://donmai.us/danbooru/trunk@1730 9af9e794-c952-11db-92b5-8e906a41d509

--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -224,33 +224,6 @@ Post = {
     })
   },
 
-  resize_image: function() {
-    var img = $("image");
-
-    if ((img.scale_factor == 1) || (img.scale_factor == null)) {
-      img.original_width = img.width;
-      img.original_height = img.height;
-      var client_width = $("right-col").clientWidth - 15;
-      var client_height = $("right-col").clientHeight;
-
-      if (img.width > client_width) {
-        var ratio = img.scale_factor = client_width / img.width;
-        img.width = img.width * ratio;
-        img.height = img.height * ratio;
-      }
-    } else {
-      img.scale_factor = 1;
-      img.width = img.original_width;
-      img.height = img.original_height;
-    }
-  
-    if (window.Note) {
-      for (var i=0; i<window.Note.all.length; ++i) {
-        window.Note.all[i].adjustScale()
-      }
-    }
-  },
-  
   highres: function() {
     var img = $("image");
     
