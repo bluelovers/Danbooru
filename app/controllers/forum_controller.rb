3eacbaaee17313ca0bc1d02582ae0dcfc9d6e02a
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2008-11-13 17:26:04 +0000

    bug fixes
    git-svn-id: svn://donmai.us/danbooru/trunk@1752 9af9e794-c952-11db-92b5-8e906a41d509

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -41,7 +41,7 @@ class ForumController < ApplicationController
         redirect_to :action => "show", :id => @forum_post.root_id
       else
         flash[:notice] = "Response posted"
-        redirect_to :action => "show", :id => @forum_post.root_id, :page => (@forum_post.root.response_count / 10.0).ceil
+        redirect_to :action => "show", :id => @forum_post.root_id, :page => (@forum_post.root.response_count / 30.0).ceil
       end
     else
       render_error(@forum_post)
@@ -85,7 +85,7 @@ class ForumController < ApplicationController
     @forum_post.attributes = params[:forum_post]
     if @forum_post.save
       flash[:notice] = "Post updated"
-      redirect_to :action => "show", :id => @forum_post.root_id, :page => (@forum_post.root.response_count / 10.0).ceil
+      redirect_to :action => "show", :id => @forum_post.root_id, :page => (@forum_post.root.response_count / 30.0).ceil
     else
       render_error(@forum_post)
     end
@@ -94,7 +94,7 @@ class ForumController < ApplicationController
   def show
     @forum_post = ForumPost.find(params[:id])
     set_title @forum_post.title
-    @children = ForumPost.paginate :order => "id", :per_page => 10, :conditions => ["parent_id = ?", params[:id]], :page => params[:page]
+    @children = ForumPost.paginate :order => "id", :per_page => 30, :conditions => ["parent_id = ?", params[:id]], :page => params[:page]
 
     if !@current_user.is_anonymous? && @current_user.last_forum_topic_read_at < @forum_post.updated_at && @forum_post.updated_at < 3.seconds.ago
       @current_user.update_attribute(:last_forum_topic_read_at, @forum_post.updated_at)
@@ -109,7 +109,7 @@ class ForumController < ApplicationController
     if params[:parent_id]
       @forum_posts = ForumPost.paginate :order => "is_sticky desc, updated_at DESC", :per_page => 100, :conditions => ["parent_id = ?", params[:parent_id]], :page => params[:page]
     else
-      @forum_posts = ForumPost.paginate :order => "is_sticky desc, updated_at DESC", :per_page => 20, :conditions => "parent_id IS NULL", :page => params[:page]
+      @forum_posts = ForumPost.paginate :order => "is_sticky desc, updated_at DESC", :per_page => 30, :conditions => "parent_id IS NULL", :page => params[:page]
     end
 
     respond_to_list("forum_posts")
@@ -118,9 +118,9 @@ class ForumController < ApplicationController
   def search
     if params[:query]
       query = params[:query].scan(/\S+/).join(" & ")
-      @forum_posts = ForumPost.paginate :order => "id desc", :per_page => 25, :conditions => ["text_search_index @@ plainto_tsquery(?)", query], :page => params[:page]
+      @forum_posts = ForumPost.paginate :order => "id desc", :per_page => 30, :conditions => ["text_search_index @@ plainto_tsquery(?)", query], :page => params[:page]
     else
-      @forum_posts = ForumPost.paginate :order => "id desc", :per_page => 25, :page => params[:page]
+      @forum_posts = ForumPost.paginate :order => "id desc", :per_page => 30, :page => params[:page]
     end
     
     respond_to_list("forum_posts")
