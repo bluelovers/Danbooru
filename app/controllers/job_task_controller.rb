f40e4ee347b25496485a9175987a73e22ae51b47
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-01-17 04:00:11 +0000

    * Added ability to delete job tasks * Favtag fixes
    git-svn-id: svn://donmai.us/danbooru/trunk@1807 9af9e794-c952-11db-92b5-8e906a41d509

--- app/controllers/job_task_controller.rb
+++ app/controllers/job_task_controller.rb
@@ -12,8 +12,17 @@ class JobTaskController < ApplicationController
       redirect_to :controller => "post", :action => "show", :id => @job_task.status_message
     end
   end
+
+  def destroy
+    @job_task = JobTask.find(params[:id])
+    
+    if request.post?
+      @job_task.destroy
+      redirect_to :action => "index"
+    end
+  end
   
-  def retry
+  def restart
     @job_task = JobTask.find(params[:id])
 
     if request.post?
