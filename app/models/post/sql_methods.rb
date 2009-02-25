e78b129bf89d8e2b9154fd24e3b707b162d1e96d
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-02-25 16:31:22 +0000

    * Bug fix
    git-svn-id: svn://donmai.us/danbooru/trunk@1904 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/post/sql_methods.rb
+++ app/models/post/sql_methods.rb
@@ -1,6 +1,7 @@
 module PostSqlMethods
   module ClassMethods
     def find_by_tag_join(tag, options = {})
+      tag = tag.downcase.tr(" ", "_")
       find(:all, :conditions => ["tags.name = ?", tag], :select => "posts.*", :joins => "JOIN posts_tags ON posts_tags.post_id = posts.id JOIN tags ON tags.id = posts_tags.tag_id", :limit => options[:limit], :offset => options[:offset], :order => (options[:order] || "posts.id DESC"))
     end
     
