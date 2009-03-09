d591af61967bdb1d8b6a370eacddf091f84764e7
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-03-09 16:54:05 +0000

    * Merged ad changes from safebooru
    git-svn-id: svn://donmai.us/danbooru/trunk@1942 9af9e794-c952-11db-92b5-8e906a41d509

--- app/controllers/advertisement_controller.rb
+++ app/controllers/advertisement_controller.rb
@@ -1,20 +1,47 @@
 class AdvertisementController < ApplicationController
-  layout "bare"
-	before_filter :admin_only, :only => [:reset_stats]
+	before_filter :admin_only, :only => [:create, :update, :new, :edit]
+  layout "default"
 
 	def redirect_ad
 		ad = Advertisement.find(params[:id])
-	  ad.increment!(:hit_count)
+    ad.hit!(request.remote_ip)
 		redirect_to ad.referral_url
 	end
 	
-	def show_stats
+	def index
 		@ads = Advertisement.find(:all, :order => "id")
-		render :layout => "default"
+
+    if params[:start_date]
+      @start_date = Date.parse(params[:start_date])
+    else
+      @start_date = 1.month.ago.to_date
+    end
+
+    if params[:end_date]
+      @end_date = Date.parse(params[:end_date])
+    else
+      @end_date = Date.today
+    end
 	end
-	
-	def reset_stats
-		Advertisement.update_all("hit_count = 0")
-		redirect_to :action => "show_stats"
-	end
+
+  def new
+    @ad = Advertisement.new
+  end
+
+  def create
+    @ad = Advertisement.create(params[:ad])
+    flash[:notice] = "Advertisement created"
+    redirect_to :action => "index"
+  end
+
+  def edit
+    @ad = Advertisement.find(params[:id])
+  end
+
+  def update
+    @ad = Advertisement.find(params[:id])
+    @ad.update_attributes(params[:ad])
+    flash[:notice] = "Advertisement updated"
+    redirect_to :action => :index
+  end
 end
--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,3 +1,12 @@
 class Advertisement < ActiveRecord::Base
   validates_inclusion_of :ad_type, :in => %w(horizontal vertical)
+  has_many :hits, :class_name => "AdvertisementHit"
+
+  def hit!(ip_addr)
+    AdvertisementHit.create(:ip_addr => ip_addr, :advertisement_id => id)
+  end
+
+  def hit_sum(start_date, end_date)
+    AdvertisementHit.count(:conditions => ["created_at BETWEEN ? AND ?", start_date, end_date])
+  end
 end
