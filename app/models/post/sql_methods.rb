634c6b080cbcc6581efeb40d946dee0c01ef376f
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-03-10 22:46:51 +0000

    * Fixes #539: Deleted posts show up in the thumbnails of wiki pages
    git-svn-id: svn://donmai.us/danbooru/trunk@1950 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/post/sql_methods.rb
+++ app/models/post/sql_methods.rb
@@ -2,7 +2,7 @@ module PostSqlMethods
   module ClassMethods
     def find_by_tag_join(tag, options = {})
       tag = tag.downcase.tr(" ", "_")
-      find(:all, :conditions => ["tags.name = ?", tag], :select => "posts.*", :joins => "JOIN posts_tags ON posts_tags.post_id = posts.id JOIN tags ON tags.id = posts_tags.tag_id", :limit => options[:limit], :offset => options[:offset], :order => (options[:order] || "posts.id DESC"))
+      find(:all, :conditions => ["tags.name = ? AND posts.status <> 'deleted'", tag], :select => "posts.*", :joins => "JOIN posts_tags ON posts_tags.post_id = posts.id JOIN tags ON tags.id = posts_tags.tag_id", :limit => options[:limit], :offset => options[:offset], :order => (options[:order] || "posts.id DESC"))
     end
     
     def generate_sql_range_helper(arr, field, c, p)
