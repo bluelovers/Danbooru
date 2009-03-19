738307158b21e98df5123775725dafc07a457757
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-03-19 22:38:17 +0000

    * FIxes #540: Remove underscores from subscription names in profile
    git-svn-id: svn://donmai.us/danbooru/trunk@1971 9af9e794-c952-11db-92b5-8e906a41d509

--- app/helpers/tag_subscription_helper.rb
+++ app/helpers/tag_subscription_helper.rb
@@ -2,7 +2,7 @@ module TagSubscriptionHelper
   def tag_subscription_listing(user)
     html = []
     user.tag_subscriptions.each do |tag_subscription|
-      html << '<span class="group"><strong>' + link_to(h(tag_subscription.name), :controller => "post", :action => "index", :tags => "sub:#{user.name}:#{tag_subscription.name}") + '</strong>: '
+      html << '<span class="group"><strong>' + link_to(h(tag_subscription.name.tr("_", " ")), :controller => "post", :action => "index", :tags => "sub:#{user.name}:#{tag_subscription.name}") + '</strong>: '
       tags = tag_subscription.tag_query.scan(/\S+/).sort
       group = []
       tags.each do |tag|
