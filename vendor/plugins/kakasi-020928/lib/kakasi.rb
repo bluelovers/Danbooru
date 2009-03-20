c04f17e3673ff16d21d96a4660cbcadb2a98b608
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-03-20 22:30:19 +0000

    * Added missing files
    git-svn-id: svn://donmai.us/danbooru/trunk@1974 9af9e794-c952-11db-92b5-8e906a41d509

--- vendor/plugins/kakasi-020928/lib/kakasi.rb
+++ vendor/plugins/kakasi-020928/lib/kakasi.rb
@@ -1,10 +1,17 @@
 module Kakasi
+  def normalize_chouon(src)
+    src.gsub(/([aiou])\s*\^/) do
+      $1 + $1
+    end.gsub(/e\s*\^/, "ei")
+  end
+  
   def utf8_kakasi(src)
     eucjp_src = Iconv.iconv("EUC-JP", "UTF-8", src)
     result = kakasi("-oeuc -ieuc -s -Ja -Ha -Ka", eucjp_src.join(""))
     utf8_src = Iconv.iconv("UTF-8", "EUC-JP", result).join("")
-    return utf8_src
+    return normalize_chouon(utf8_src)
   end
   
+  module_function :normalize_chouon
   module_function :utf8_kakasi
 end
