e0660c1f6d4d9e8459df227f6daef7a479d0fa55
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-05-06 23:02:14 +0000

    additional fast_link_to changes
    git-svn-id: svn://donmai.us/danbooru/trunk@2100 9af9e794-c952-11db-92b5-8e906a41d509

--- vendor/plugins/will_paginate/lib/will_paginate/view_helpers.rb
+++ vendor/plugins/will_paginate/lib/will_paginate/view_helpers.rb
@@ -168,7 +168,7 @@ module WillPaginate
     def page_link_or_span(page, span_class = 'current', text = nil)
       text ||= page.to_s
       if page and page != current_page
-        @template.link_to text, url_options(page)
+        @template.fast_link_to text, url_options(page)
       else
         @template.content_tag :span, text, :class => span_class
       end
