4a5ee80bbe048b85249ef10b6ac282a0f22eee2e
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-05-06 23:23:44 +0000

    removed to_json call on posts partial
    git-svn-id: svn://donmai.us/danbooru/trunk@2102 9af9e794-c952-11db-92b5-8e906a41d509

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -15,23 +15,19 @@ module PostHelper
       return ""
     end
 
-    image_class = "preview"
-    image_class += " flagged" if post.is_flagged?
-    image_class += " pending" if post.is_pending?
-    image_class += " has-children" if post.has_children?
-    image_class += " has-parent" if post.parent_id
-    image_id = options[:image_id]
-    image_id = %{id="#{h(image_id)}"} if image_id
-    image_title = h(post.cached_tags + " rating:#{post.pretty_rating} score:#{post.score} user:#{post.author}")
     link_onclick = options[:onclick]
     link_onclick = %{onclick="#{link_onclick}"} if link_onclick
     width, height = post.preview_dimensions
-
-    image = %{<img src="#{post.preview_url}" alt="#{image_title}" class="#{image_class}" title="#{image_title}" #{image_id} width="#{width}" height="#{height}">}
-    plid = %{<span class="plid">#pl http://#{h CONFIG["server_host"]}/post/show/#{post.id}</span>}
-    link = %{<a href="/post/show/#{post.id}/#{u(post.tag_title)}" #{link_onclick}>#{image}#{plid}</a>}
-    span = %{<span class="thumb" id="p#{post.id}">#{link}</span>}
-    return span
+    image_id = options[:image_id]
+    image_id = %{id="#{h(image_id)}"} if image_id
+    
+    %{
+      <span class="thumb" id="p#{post.id}">
+        <a href="/post/show/#{post.id}/#{u(post.tag_title)}" #{link_onclick}>
+          <img #{image_id} class="preview #{'flagged' if post.is_flagged?} #{'pending' if post.is_pending?} #{'has-children' if post.has_children?} #{'has-parent' if post.parent_id}" src="#{post.preview_url}" alt="#{h(post.cached_tags)} rating:#{post.pretty_rating} score:#{post.score} user:#{h(post.author)}" width=#{width} height=#{height}>
+        </a>
+      </span>
+    }
   end
   
   def print_tag_sidebar_helper(tag)
