a4f158a8f0c4e5bebf98eb61317fa067a50f6347
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-08-14 23:01:52 +0000

    * Bug fixes
    git-svn-id: svn://donmai.us/danbooru/trunk@2204 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -225,10 +225,17 @@ class Artist < ActiveRecord::Base
   
   module VersionMethods
     def self.included(m)
+      m.before_save :initialize_version
       m.after_save :create_version
-      m.has_many :versions, :class_name => "ArtistVersion", :order => "version desc"
+      m.has_many :versions, :class_name => "ArtistVersion", :order => "version desc", :dependent => :delete_all
     end
     
+    def initialize_version
+      if version.nil?
+        self.version = 1
+      end
+    end
+
     def create_version
       cached_urls = artist_urls.map {|x| x.normalized_url}.join(" ")
       
