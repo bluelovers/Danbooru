09817b427b92ba6cf794b9b2b7201ba12e0ef2e3
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-09-02 14:59:45 +0000

    * Added new s3 backup job task
    git-svn-id: svn://donmai.us/danbooru/trunk@2222 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/job_task.rb
+++ app/models/job_task.rb
@@ -1,5 +1,5 @@
 class JobTask < ActiveRecord::Base
-  TASK_TYPES = %w(bandwidth_throttle mass_tag_edit approve_tag_alias approve_tag_implication calculate_tag_subscriptions calculate_related_tags calculate_post_count calculate_uploaded_tags)
+  TASK_TYPES = %w(bandwidth_throttle mass_tag_edit approve_tag_alias approve_tag_implication calculate_tag_subscriptions calculate_related_tags calculate_post_count calculate_uploaded_tags s3_backup)
   STATUSES = %w(pending processing finished error)
   
   validates_inclusion_of :task_type, :in => TASK_TYPES
@@ -100,6 +100,27 @@ class JobTask < ActiveRecord::Base
     Cache.put("db-bw", diff)
   end
   
+  def execute_s3_backup
+    last_id = data["last_id"].to_i
+    
+    Post.find(:all, :conditions => ["id > ?", last_id], :limit => 100, :order => "id").each do |post|
+      base64_md5 = Base64.encode64(post.md5.unpack("a2" * (post.md5.size / 2)).map {|x| x.hex.chr}.join)
+  
+      AWS::S3::Base.establish_connection!(:access_key_id => CONFIG["amazon_s3_access_key_id"], :secret_access_key => CONFIG["amazon_s3_secret_access_key"])
+      AWS::S3::S3Object.store(post.file_name, open(post.file_path, "rb"), CONFIG["amazon_s3_bucket_name"], "Content-MD5" => base64_md5)
+  
+      if post.image?
+        AWS::S3::S3Object.store("preview/#{post.md5}.jpg", open(post.preview_path, "rb"), CONFIG["amazon_s3_bucket_name"])
+      end
+
+      if File.exists?(post.sample_path)
+        AWS::S3::S3Object.store("sample/" + CONFIG["sample_filename_prefix"] + "#{post.md5}.jpg", open(post.sample_path, "rb"), CONFIG["amazon_s3_bucket_name"])
+      end
+      
+      update_attributes(:data => {:last_id => post.id})
+    end
+  end
+  
   def pretty_data
     begin
       case task_type
@@ -137,6 +158,10 @@ class JobTask < ActiveRecord::Base
         
       when "bandwidth_throttle"
         ""
+        
+      when "s3_backup"
+        "last_id:" + data["last_id"].to_s
+        
       end
     rescue Exception
       "ERROR"
---
+++ script/fix/002_create_s3_backup_job.rb
@@ -0,0 +1,1 @@
+JobTask.create(:task_type => "s3_backup", :repeat_count => -1, :data => {:last_id => 0}, :status => "pending")
