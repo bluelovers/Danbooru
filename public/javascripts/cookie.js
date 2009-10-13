9415cf39be2829a642ec37c464d1b82042ff7bc6
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2009-10-13 21:12:20 +0000

    * Applies #1025: Non-ASCII characters break tag scripts
    git-svn-id: svn://donmai.us/danbooru/trunk@2264 9af9e794-c952-11db-92b5-8e906a41d509

--- public/javascripts/cookie.js
+++ public/javascripts/cookie.js
@@ -7,7 +7,7 @@ Cookie = {
     var date = new Date()
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
     var expires = "; expires=" + date.toGMTString()
-    document.cookie = name + "=" + escape(value) + expires + "; path=/"
+    document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/"
   },
 
   raw_get: function(name) {
@@ -38,7 +38,7 @@ Cookie = {
   },
 
   unescape: function(val) {
-    return unescape(val.replace(/\+/g, " "))
+    return decodeURIComponent(val.replace(/\+/g, " "))
   },
 
   setup: function() {
