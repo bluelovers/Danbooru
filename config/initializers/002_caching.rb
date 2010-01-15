d39d8ce82622e7b2bce1fcb8e994b2da141658f8
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2010-01-15 17:12:10 +0000

    fixes #1138
    git-svn-id: svn://donmai.us/danbooru/trunk@2405 9af9e794-c952-11db-92b5-8e906a41d509

--- config/initializers/002_caching.rb
+++ config/initializers/002_caching.rb
@@ -1,4 +1,4 @@
 unless defined?(MEMCACHE)
-  MEMCACHE = MemCache.new :c_threshold => 10_000, :compression => true, :debug => false, :namespace => CONFIG["app_name"], :readonly => false, :urlencode => false
+  MEMCACHE = MemCache.new :c_threshold => 10_000, :compression => true, :debug => false, :namespace => CONFIG["app_name"].gsub(/[^A-Za-z0-9]/, "_"), :readonly => false, :urlencode => false
   MEMCACHE.servers = CONFIG["memcache_servers"]
 end
