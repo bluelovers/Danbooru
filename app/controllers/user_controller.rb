23be61c02ab61d6e627a76d671c5d2163bb0a432
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2010-01-20 22:00:32 +0000

    hide user/show_advanced to anon users
    git-svn-id: svn://donmai.us/danbooru/trunk@2431 9af9e794-c952-11db-92b5-8e906a41d509

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,7 @@ class UserController < ApplicationController
   layout "default"
   verify :method => :post, :only => [:authenticate, :update, :create]
   before_filter :blocked_only, :only => [:authenticate, :update, :edit]
+  before_filter :member_only, :only => [:show_advanced]
   before_filter :janitor_only, :only => [:invites, :revert_tag_changes, :block, :unblock, :show_blocked_users]
   before_filter :admin_only, :only => [:edit_upload_limit, :update_upload_limit]
   helper :post, :tag_subscription
