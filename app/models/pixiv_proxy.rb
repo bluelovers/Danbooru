5592251c5e1cbf62e749ab3b6e50822840fb0b74
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2010-07-25 01:18:17 +0000

    updated PixivProxy for new pixiv site changes, added tests
    git-svn-id: svn://donmai.us/danbooru/trunk@2513 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/pixiv_proxy.rb
+++ app/models/pixiv_proxy.rb
@@ -23,7 +23,7 @@ class PixivProxy < ActiveRecord::Base
 		mech = create_mechanize
 		hash = {}
 		mech.get(url) do |page|
-			hash[:artist] = page.search("div#profile/div/a/img").attr("alt")
+			hash[:artist] = page.search("a.avatar_m").attr("title").value
 			hash[:listing_url] = "/member_illust.php?id=" + url[/id=(\d+)/, 1]
 		end
 		hash
@@ -34,11 +34,11 @@ class PixivProxy < ActiveRecord::Base
 		mech = create_mechanize
 		hash = {}
 		mech.get(url) do |page|
-		  if page.search("div#profile/div/a/img")
-  			hash[:artist] = page.search("div#profile/div/a/img").attr("alt").value
-  			hash[:image_url] = page.search("div#profile/div/a/img").attr("src").value.sub("_m.", ".")
-  			hash[:profile_url] = page.search("div#profile/div/a").attr("href").value
-  			hash[:jp_tags] = page.search("div#tag_area/span#tags/a").map do |node|
+		  if page.search("a.avatar_m")
+  			hash[:artist] = page.search("a.avatar_m").attr("title").value
+  			hash[:image_url] = page.search("div.works_display/a/img").attr("src").value.sub("_m.", ".")
+  			hash[:profile_url] = page.search("a.avatar_m").attr("href").value
+  			hash[:jp_tags] = page.search("span#tags/a").map do |node|
   				[node.inner_text, node.attribute("href").to_s]
   			end.reject {|x| x[0].empty?}
 			else
@@ -51,39 +51,11 @@ class PixivProxy < ActiveRecord::Base
 	  hash
 	end
 	
-	def self.get_listing(url)
-		mech = create_mechanize
-		p = 1
-		url = URI.parse(url).request_uri.sub(/&p=\d+/, "") + "&p=1"
-		more = true
-		images = []
-		
-		while more
-			mech.get(url) do |page|
-				links = page.search("div#illust_c4/ul/li/a")
-				
-				if links.empty?
-					more = false
-				else
-					images += links.map do |node|
-						image_src = node.child.attribute("src").to_s
-						[image_src, image_src.sub("_s.", "."), node.attribute("href").to_s]
-					end
-				end
-				
-				p += 1
-				url.sub!(/&p=\d+/, "&p=#{p}")
-			end
-		end
-		
-		images
-	end
-
 	def self.create_mechanize
-		mech = WWW::Mechanize.new
+		mech = Mechanize.new
 		
 		mech.get("http://www.pixiv.net") do |page|
-			page.form_with(:action => "index.php") do |form|
+			page.form_with(:action => "/index.php") do |form|
 				form.pixiv_id = "uroobnad"
 				form.pass = "uroobnad556"
 			end.click_button
--- test/unit/pixiv_proxy_test.rb
+++ test/unit/pixiv_proxy_test.rb
@@ -1,8 +1,19 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class PixivProxyTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def test_get_profile
+    results = PixivProxy.get_profile("http://www.pixiv.net/member_illust.php?mode=medium&illust_id=9646484")
+    assert_equal("シビレ\347\275\240", results[:artist])
+    assert_equal("/member_illust.php?id=9646484", results[:listing_url])
+  end
+  
+  def test_get_single
+    results = PixivProxy.get_single("http://www.pixiv.net/member_illust.php?mode=medium&illust_id=9646484")
+    assert_equal("/member.php?id=4015", results[:profile_url])
+    assert(results[:jp_tags].size > 0)
+    first_tag = results[:jp_tags][0]
+    assert_equal(2, first_tag.size)
+    assert(first_tag[0] =~ /./)
+    assert(first_tag[1] =~ /tags\.php\?tag=/)
   end
 end
