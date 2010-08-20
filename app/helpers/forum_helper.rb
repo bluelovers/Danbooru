063ccfc2edf3b2202d1b4a57491b0f0261c16cd0
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2010-08-20 17:31:39 +0000

    added alias/implication convenience links
    git-svn-id: svn://donmai.us/danbooru/trunk@2522 9af9e794-c952-11db-92b5-8e906a41d509

--- app/helpers/forum_helper.rb
+++ app/helpers/forum_helper.rb
@@ -1,2 +1,15 @@
 module ForumHelper
+  def auto_link_tag_changes(forum_topic, text)
+    if @current_user.is_admin?
+      text = text.gsub(/tag alias:\s*(\S+) -+(?:>|&gt;) (\S+)/i) do
+        %{Tag Alias: #{$1} -> #{$2} [<a href="/tag_alias/index?from_name=#{$1}&to_name=#{$2}&reason=See+forum+%23#{forum_topic.id}">create</a>]}
+      end
+    
+      text = text.gsub(/tag implication:\s*(\S+) -+(?:>|&gt;) (\S+)/i) do
+        %{Tag Implication: #{$1} -> #{$2} [<a href="/tag_implication/index?from_name=#{$1}&to_name=#{$2}&reason=See+forum+%23#{forum_topic.id}">create</a>]}
+      end
+    end
+    
+    text
+  end
 end
