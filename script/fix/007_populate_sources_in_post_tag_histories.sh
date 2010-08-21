e368adbcd31577c55e77bc7bafe46eb91d403cb0
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2010-08-21 16:30:13 +0000

    fix
    git-svn-id: svn://donmai.us/danbooru/trunk@2527 9af9e794-c952-11db-92b5-8e906a41d509

--- script/fix/007_populate_sources_in_post_tag_histories.sh
+++ script/fix/007_populate_sources_in_post_tag_histories.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-psql -hdbserver -c "UPDATE post_tag_histories SET source = (SELECT source FROM posts WHERE id = post_id)" danbooru 
+PGOPTIONS="-c statement_timeout=0" psql -hdbserver -c "UPDATE post_tag_histories SET source = (SELECT source FROM posts WHERE id = post_id)" danbooru 
