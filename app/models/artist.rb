72608bda8be827b8869829047348b1a313cbbbd9
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2011-01-28 21:42:32 +0000

    consolidated artist model
    git-svn-id: svn://donmai.us/danbooru/trunk@2561 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,13 +1,177 @@
 class Artist < ActiveRecord::Base
   default_scope :select => "artists.*, coalesce(array_to_string(other_names_array, ', '), '') AS other_names_string"
 
-  include ArtistMethods::UrlMethods
-  include ArtistMethods::NoteMethods
-  include ArtistMethods::OtherNameMethods
-  include ArtistMethods::GroupMethods
-  include ArtistMethods::ApiMethods
-  include ArtistMethods::VersionMethods
+  module ApiMethods
+    def api_attributes
+      return {
+        :id => id, 
+        :name => name, 
+        :other_names => other_names,
+        :group_name => group_name,
+        :urls => artist_urls.map {|x| x.url},
+        :is_active => is_active,
+        :version => version,
+        :updater_id => updater_id
+      }
+    end
+
+    def to_xml(options = {})
+      attribs = api_attributes
+      attribs[:urls] = attribs[:urls].join(" ")
+      attribs.to_xml(options.merge(:root => "artist"))
+    end
+
+    def to_json(*args)
+      return api_attributes.to_json(*args)
+    end
+  end
   
+  module GroupMethods
+    def member_names
+      members.map(&:name).join(", ")
+    end
+  end
+  
+  module NoteMethods
+    def wiki_page
+      WikiPage.find_page(name)
+    end
+
+    def notes_locked?
+      wiki_page.is_locked? rescue false
+    end
+
+    def notes
+      wiki_page.body rescue ""
+    end
+
+    def notes=(text)
+      @notes = text
+    end
+    
+    def commit_notes
+      unless @notes.blank?
+        if wiki_page.nil?
+          WikiPage.create(:title => name, :body => @notes, :ip_addr => updater_ip_addr, :user_id => updater_id)
+        elsif wiki_page.is_locked?
+          errors.add(:notes, "are locked")
+        else
+          wiki_page.update_attributes(:body => @notes, :ip_addr => updater_ip_addr, :user_id => updater_id)
+        end
+      end
+    end
+  end
+  
+  module OtherNameMethods
+    def initialize_other_names
+      if @other_names
+        self.other_names_array = "{" + @other_names.split(/,/).map do |x|
+          sanitized_name = x.gsub(/\\/, "\\\\\\\\").gsub(/"/, "\\\\\"").strip.gsub(/\s/, "_")
+          
+          %{"#{sanitized_name}"}
+        end.join(",") + "}"
+      end
+    end
+    
+    def other_names=(x)
+      @other_names = x
+    end
+    
+    def other_names
+      if self["other_names_string"]
+        self.other_names_string
+      else
+        nil
+      end
+    end
+  end
+  
+  module UrlMethods
+    module ClassMethods
+      def find_all_by_url(url)
+        url = ArtistUrl.normalize(url)
+        artists = []
+
+        while artists.empty? && url.size > 10
+          u = url.gsub(/\/+$/, "") + "/"
+          u = u.to_escaped_for_sql_like.gsub(/\*/, '%') + '%'
+          artists += Artist.find(:all, :joins => "JOIN artist_urls ON artist_urls.artist_id = artists.id", :conditions => ["artists.is_active = TRUE AND artist_urls.normalized_url LIKE ? ESCAPE E'\\\\'", u], :order => "artists.name", :limit => "5")
+
+          # Remove duplicates based on name
+          artists = artists.inject({}) {|all, artist| all[artist.name] = artist ; all}.values
+          url = File.dirname(url) + "/"
+        end
+
+        if artists.size > 3
+          return []
+        else
+          return artists[0, 20]
+        end
+      end
+    end
+    
+    def self.included(m)
+      m.extend(ClassMethods)
+    end
+    
+    def commit_urls
+      if @urls
+        artist_urls.clear
+
+        @urls.scan(/\S+/).each do |url|
+          artist_urls.create(:url => url)
+        end
+      end
+    end
+    
+    def urls=(urls)
+      @urls = urls
+    end
+    
+    def urls
+      @urls || artist_urls.map {|x| x.url}.join("\n")
+    end
+  end
+  
+  module VersionMethods
+    def initialize_version
+      if version.nil?
+        self.version = 1
+      end
+    end
+
+    def create_version
+      cached_urls = artist_urls.map {|x| x.normalized_url}.join(" ")
+      
+      ArtistVersion.create(
+        :artist_id => id,
+        :version => version,
+        :name => name,
+        :updater_id => updater_id,
+        :cached_urls => cached_urls,
+        :is_active => is_active,
+        :other_names_array => other_names_array,
+        :group_name => group_name
+      )
+      
+      Artist.execute_sql "UPDATE artists SET version = version + 1 WHERE id = #{id}"
+    end
+  end
+  
+  include UrlMethods
+  include NoteMethods
+  include OtherNameMethods
+  include GroupMethods
+  include ApiMethods
+  include VersionMethods
+  
+  after_save :commit_notes
+  before_save :initialize_other_names
+  after_save :commit_urls
+  has_many :artist_urls, :dependent => :delete_all
+  before_save :initialize_version
+  after_save :create_version
+  has_many :versions, :class_name => "ArtistVersion", :order => "version desc", :dependent => :delete_all
   before_validation :normalize
   validates_uniqueness_of :name
   validates_format_of :name, :with => /\S/
--- app/models/artist_methods/group_methods.rb
+++
@@ -1,7 +0,0 @@
-module ArtistMethods
-  module GroupMethods
-    def member_names
-      members.map(&:name).join(", ")
-    end
-  end
-end
