5b5c5b17f315015d3a956cc94ad2f1bc80efedfc
albert <albert@9af9e794-c952-11db-92b5-8e906a41d509>
2011-05-02 00:48:44 +0000

    bug fix
    git-svn-id: svn://donmai.us/danbooru/trunk@2625 9af9e794-c952-11db-92b5-8e906a41d509

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,9 @@
 class Favorite < ActiveRecord::Base
   def self.build_sql_order_clause(posts_table_alias, post_ids)
+    if post_ids.empty?
+      return "#{posts_table_alias}.id desc"
+    end
+
     conditions = []
     
     post_ids.each_with_index do |post_id, n|
